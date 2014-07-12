9939cfc4d3b0da6e32df989bf6e4909c80ff85f0
Shannon Tsaltas <shannontsaltas@gmail.com>
2014-07-12 17:13:31 -0500

    implemented authorization with Twitter API

--- tweetful.py
+++ tweetful.py
@@ -1,6 +1,13 @@
+import authorization
+import requests
+
+from urls import *
+
 def main():
-    """ Main function """
-    pass
+    auth = authorization.authorize()
+
+    response = requests.get(TIMELINE_URL, auth=auth)
+    print response.json()
 
 if __name__ == "__main__":
     main()
